9d8f521db6ee32504c0e47a7abc4f4b46fb87aa0
FridaOwl <farvin.lori@gmail.com>
2024-05-27 00:28:55 +0300

    upd:polish

--- src/model/polish_notation.cc
+++ src/model/polish_notation.cc
@@ -4,124 +4,130 @@ namespace MyCalculator {
 
 const std::map<std::string, char> MyCalculator::PolishNotation::functions = {
     {"sin", 's'},  {"cos", 'c'},  {"tan", 't'}, {"asin", 'i'}, {"acos", 'o'},
-    {"atan", 'n'}, {"sqrt", 'q'}, {"ln", 'l'},  {"log", 'g'}
-};
+    {"atan", 'n'}, {"sqrt", 'q'}, {"ln", 'l'},  {"log", 'g'}};
 
 std::string PolishNotation::toPostfix(const std::string &infix) {
-    std::stack<char> operatorStack;
-    std::string postfix = "";
-    std::map<char, int> Priority = {{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2},
-                                    {'%', 2}, {'u', 4}, {'^', 5}};
-    
-    MyCalculator::StringValidator checkString(infix);
-    if (!checkString.IsValid()) {
-        throw std::invalid_argument("The equation is incorrect!");
-    }
+  std::stack<char> operatorStack;
+  std::string postfix = "";
+  std::map<char, int> Priority = {{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2},
+                                  {'%', 2}, {'u', 4}, {'^', 5}};
 
-    for (size_t i = 0; i < infix.size(); i++) {
-        char c = infix[i];
-        if (std::isdigit(c) || c == '.') {
-            findNumeric(infix, i, postfix);
-        } else if (c == '(') {
-            operatorStack.push(c);
-        } else if (c == ')') {
-            handleClosingParenthesis(operatorStack, postfix);
-        } else if (c == '-') {
-            if (i == 0 || infix[i - 1] == '(' || operation(infix[i - 1])) {
-                operatorStack.push('u');
-            } else {
-                handleOperator(c, operatorStack, postfix);
-            }
-        } else if (operation(c)) {
-            handleOperator(c, operatorStack, postfix);
-        } else if (std::isalpha(c)) {
-            handleFunction(infix, i, operatorStack);
-        }
-    }
+  MyCalculator::StringValidator checkString(infix);
+  if (!checkString.IsValid()) {
+    throw std::invalid_argument("The equation is incorrect!");
+  }
 
-    while (!operatorStack.empty()) {
-        postfix += operatorStack.top();
-        postfix += ' ';
-        operatorStack.pop();
+  for (size_t i = 0; i < infix.size(); i++) {
+    char c = infix[i];
+    if (std::isdigit(c) || c == '.') {
+      findNumeric(infix, i, postfix);
+    } else if (c == '(') {
+      operatorStack.push(c);
+    } else if (c == ')') {
+      handleClosingParenthesis(operatorStack, postfix);
+    } else if (c == '-') {
+      if (i == 0 || infix[i - 1] == '(' || operation(infix[i - 1])) {
+        operatorStack.push('u');
+      } else {
+        handleOperator(c, operatorStack, postfix);
+      }
+    } else if (operation(c)) {
+      handleOperator(c, operatorStack, postfix);
+    } else if (std::isalpha(c)) {
+      handleFunction(infix, i, operatorStack);
     }
+  }
 
-    return postfix;
+  while (!operatorStack.empty()) {
+    postfix += operatorStack.top();
+    postfix += ' ';
+    operatorStack.pop();
+  }
+
+  return postfix;
 }
 
-void PolishNotation::findNumeric(const std::string &infix, size_t &index, std::string &postfix) {
-    while (index < infix.size() &&
-           (std::isdigit(infix[index]) || infix[index] == '.' ||
-            infix[index] == 'e' || infix[index] == 'E')) {
-        postfix += infix[index];
-        if ((infix[index] == 'e' || infix[index] == 'E') &&
-            index + 1 < infix.size() &&
-            (infix[index + 1] == '+' || infix[index + 1] == '-')) {
-            postfix += infix[++index];
-            if (index + 1 < infix.size() && std::isdigit(infix[index + 1])) {
-                postfix += infix[++index];
-            } else {
-                throw std::invalid_argument("Incorrect number");
-            }
-        }
-        index++;
+void PolishNotation::findNumeric(const std::string &infix, size_t &index,
+                                 std::string &postfix) {
+  while (index < infix.size() &&
+         (std::isdigit(infix[index]) || infix[index] == '.' ||
+          infix[index] == 'e' || infix[index] == 'E')) {
+    postfix += infix[index];
+    if ((infix[index] == 'e' || infix[index] == 'E') &&
+        index + 1 < infix.size() &&
+        (infix[index + 1] == '+' || infix[index + 1] == '-')) {
+      postfix += infix[++index];
+      if (index + 1 < infix.size() && std::isdigit(infix[index + 1])) {
+        postfix += infix[++index];
+      } else {
+        throw std::invalid_argument("Incorrect number");
+      }
     }
-    postfix += ' ';
-    index--;
+    index++;
+  }
+  postfix += ' ';
+  index--;
 }
 
 bool PolishNotation::operation(char c) {
-    return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%' || c == 'u';
+  return c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%' ||
+         c == 'u';
 }
 
 int PolishNotation::priorityOperation(char c) {
-    switch (c) {
-        case '+':
-        case '-':
-            return 1;
-        case '*':
-        case '/':
-        case '%':
-            return 2;
-        case 'u':
-            return 4;
-        case '^':
-            return 5;
-        default:
-            return 6;
-    }
+  switch (c) {
+    case '+':
+    case '-':
+      return 1;
+    case '*':
+    case '/':
+    case '%':
+      return 2;
+    case 'u':
+      return 4;
+    case '^':
+      return 5;
+    default:
+      return 6;
+  }
 }
 
-void PolishNotation::handleClosingParenthesis(std::stack<char> &operatorStack, std::string &postfix) {
-    while (!operatorStack.empty() && operatorStack.top() != '(') {
-        postfix += operatorStack.top();
-        postfix += ' ';
-        operatorStack.pop();
-    }
-    if (!operatorStack.empty()) {
-        operatorStack.pop();
-    }
+void PolishNotation::handleClosingParenthesis(std::stack<char> &operatorStack,
+                                              std::string &postfix) {
+  while (!operatorStack.empty() && operatorStack.top() != '(') {
+    postfix += operatorStack.top();
+    postfix += ' ';
+    operatorStack.pop();
+  }
+  if (!operatorStack.empty()) {
+    operatorStack.pop();
+  }
 }
 
-void PolishNotation::handleOperator(char c, std::stack<char> &operatorStack, std::string &postfix) {
-    while (!operatorStack.empty() && operatorStack.top() != '(' &&
-           (c != '^' ? priorityOperation(operatorStack.top()) >= priorityOperation(c)
-                     : priorityOperation(operatorStack.top()) > priorityOperation(c))) {
-        postfix += operatorStack.top();
-        postfix += ' ';
-        operatorStack.pop();
-    }
-    operatorStack.push(c);
+void PolishNotation::handleOperator(char c, std::stack<char> &operatorStack,
+                                    std::string &postfix) {
+  while (
+      !operatorStack.empty() && operatorStack.top() != '(' &&
+      (c != '^'
+           ? priorityOperation(operatorStack.top()) >= priorityOperation(c)
+           : priorityOperation(operatorStack.top()) > priorityOperation(c))) {
+    postfix += operatorStack.top();
+    postfix += ' ';
+    operatorStack.pop();
+  }
+  operatorStack.push(c);
 }
 
-void PolishNotation::handleFunction(const std::string &infix, size_t &index, std::stack<char> &operatorStack) {
-    std::string fx = "";
-    while (index < infix.size() && std::isalpha(infix[index])) {
-        fx += infix[index++];
-    }
-    --index;
-    if (functions.find(fx) != functions.end()) {
-        operatorStack.push(functions.at(fx));
-    }
+void PolishNotation::handleFunction(const std::string &infix, size_t &index,
+                                    std::stack<char> &operatorStack) {
+  std::string fx = "";
+  while (index < infix.size() && std::isalpha(infix[index])) {
+    fx += infix[index++];
+  }
+  --index;
+  if (functions.find(fx) != functions.end()) {
+    operatorStack.push(functions.at(fx));
+  }
 }
 
 }  // namespace MyCalculator
--- src/model/polish_notation.h
+++ src/model/polish_notation.h
@@ -16,14 +16,18 @@ class PolishNotation {
  public:
   static std::string toPostfix(const std::string &infix);
   static const std::map<std::string, char> functions;
-  
+
  private:
-  static void findNumeric(const std::string &infix, size_t &index, std::string &postfix);
+  static void findNumeric(const std::string &infix, size_t &index,
+                          std::string &postfix);
   static bool operation(char c);
   static int priorityOperation(char c);
-  static void handleClosingParenthesis(std::stack<char> &operatorStack, std::string &postfix);
-  static void handleOperator(char c, std::stack<char> &operatorStack, std::string &postfix);
-  static void handleFunction(const std::string &infix, size_t &index, std::stack<char> &operatorStack);
+  static void handleClosingParenthesis(std::stack<char> &operatorStack,
+                                       std::string &postfix);
+  static void handleOperator(char c, std::stack<char> &operatorStack,
+                             std::string &postfix);
+  static void handleFunction(const std::string &infix, size_t &index,
+                             std::stack<char> &operatorStack);
 };
 
 }  // namespace MyCalculator
